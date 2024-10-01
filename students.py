4410a4b0fb6d46ea13331eabab8454eaefaac281
Ivan Nikitin <asd2211@mail.ru>
2024-10-01 17:34:43 +0800

    task 1,2 and half of task 3 of homework

--- students.py
+++ students.py
@@ -1,17 +1,44 @@
+from itertools import chain
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
-
 class Lecture(Mentor):
     def __init__(self, name, surname, rates = 0):
         super().__init__(name, surname)
         self.rates = {}
+
     def __str__(self):
-        return (f"Имя: {self.name} \n Фамилия {self.surname} \n Средняя оценка за лекции {sum(self.rates.values())/len(self.rates)}")
+        return (f"Имя: {self.name} \nФамилия {self.surname} \nСредняя оценка за лекции {sum(self.rates.values())/len(self.rates)}")
         #return f"Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {sum(self.rates.values())}"
+
+    def __eq__(self, other):
+        if isinstance(other, Lecture):
+            return sum(self.rates.values())/len(self.rates) == sum(other.rates.values())/len(other.rates)
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, Lecture):
+            return sum(self.rates.values())/len(self.rates) < sum(other.rates.values())/len(other.rates)
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, Lecture):
+            return sum(self.rates.values()) / len(self.rates) <= sum(other.rates.values()) / len(other.rates)
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Lecture):
+            return sum(self.rates.values()) / len(self.rates) > sum(other.rates.values()) / len(other.rates)
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, Lecture):
+            return sum(self.rates.values()) / len(self.rates) >= sum(other.rates.values()) / len(other.rates)
+        return NotImplemented
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -25,6 +52,8 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}"
 
 class Student:
     def __init__(self, name, surname, gender):
@@ -40,11 +69,6 @@ class Student:
 
     def rate_lecture(self, lecture: Lecture, course, rate):
         if 0 <= rate <= 10:
-            # for l in lecture.courses_attached:
-            #     if l == course:
-            #         Lecture.rates = {course: rate}
-            #     else:
-            #         print(f"Этот преподователь не ведет данный курс")
             if course in lecture.courses_attached and course in self.courses_in_progress:
                 if course in lecture.rates:
                     average_rate = (sum(lecture.rates.values())+rate) / (len(lecture.rates)+1)
@@ -57,23 +81,105 @@ class Student:
         else:
             print(f"оценки выставляются по 10-балльной шкале, вы ввели {rate}")
 
+    def __str__(self):
+        all_grades = list(chain.from_iterable(self.grades.values()))
+        average_rate = sum(all_grades)/len(self.grades)
+        result = (f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашнее задание:{average_rate }\n'
+                   f'Курсы в процессе{self.courses_in_progress}\nЗавершенные курсы:{self.finished_courses}')
+        return result
+
+    def __eq__(self, other):
+        if isinstance(other, Student):
+            all_grades = list(chain.from_iterable(self.grades.values()))
+            average_rate = sum(all_grades) / len(self.grades)
+            other_all_grades = list(chain.from_iterable(other.grades.values()))
+            other_average_rate = sum(all_grades) / len(other.grades)
+            return all_grades == other_all_grades
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, Student):
+            all_grades = list(chain.from_iterable(self.grades.values()))
+            average_rate = sum(all_grades) / len(self.grades)
+            other_all_grades = list(chain.from_iterable(other.grades.values()))
+            other_average_rate = sum(all_grades) / len(other.grades)
+            return all_grades < other_all_grades
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, Student):
+            all_grades = list(chain.from_iterable(self.grades.values()))
+            average_rate = sum(all_grades) / len(self.grades)
+            other_all_grades = list(chain.from_iterable(other.grades.values()))
+            other_average_rate = sum(all_grades) / len(other.grades)
+            return all_grades <= other_all_grades
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Student):
+            all_grades = list(chain.from_iterable(self.grades.values()))
+            average_rate = sum(all_grades) / len(self.grades)
+            other_all_grades = list(chain.from_iterable(other.grades.values()))
+            other_average_rate = sum(all_grades) / len(other.grades)
+            return all_grades > other_all_grades
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, Student):
+            all_grades = list(chain.from_iterable(self.grades.values()))
+            average_rate = sum(all_grades) / len(self.grades)
+            other_all_grades = list(chain.from_iterable(other.grades.values()))
+            other_average_rate = sum(all_grades) / len(other.grades)
+            return all_grades >= other_all_grades
+        return NotImplemented
+
+
+
+delimiter = "******************************************"
 student1 = Student('Ivan', 'Nikitin', 'male')
 student2 = Student('Gwido', 'van Rossum', 'male')
 student1.courses_in_progress += ['Python']
+student1.finished_courses += ['HTML']
 student2.courses_in_progress += ['Linux']
 student2.courses_in_progress += ['Python']
 student2.courses_in_progress += ['FreeBSD']
+student2.finished_courses += ['Network']
 lecture1 = Lecture("Mark", "Lutz")
 lecture2 = Lecture("Michael", "Lucas")
 lecture1.courses_attached += ['Python']
 lecture2.courses_attached += ['Linux']
 lecture2.courses_attached += ['FreeBSD']
+reviewer1 = Reviewer("Bjarne", "Staroustrup")
+reviewer2 = Reviewer("Richard", "Stallman")
+reviewer1.courses_attached += ['Python']
+reviewer1.courses_attached += ['Linux']
+reviewer1.courses_attached += ['FreeBSD']
+reviewer1.courses_attached += ['HTML']
+reviewer2.courses_attached += ['HTML']
+reviewer2.courses_attached += ['Python']
+reviewer2.courses_attached += ['Linux']
 student1.rate_lecture(lecture1, 'Python', 10)
 student2.rate_lecture(lecture1, 'Python', 3)
 student1.rate_lecture(lecture2, 'Python', 10)
 student2.rate_lecture(lecture2, 'Linux', 3)
 student2.rate_lecture(lecture2, 'FreeBSD', 8)
-print(lecture1.rates)
-print(lecture2.rates)
-print(lecture1)
-print(lecture2)
+reviewer1.rate_hw(student1, 'Python', 7)
+reviewer2.rate_hw(student1, 'HTML', 8)
+reviewer1.rate_hw(student2, 'Python', 9)
+reviewer2.rate_hw(student2, 'Linux', 6)
+# print(lecture1.rates)
+# print(lecture2.rates)
+# print(delimiter)
+# print(lecture1)
+# print(lecture2)
+# print(delimiter)
+# print(reviewer1)
+# print(reviewer2)
+# print(delimiter)
+# print(student1)
+# print(student2)
+print (lecture2 < lecture1)
+print (lecture2 > lecture1)
+print(student1 == student2)
+print(student1 > student2)
+print(student1 < student2)
